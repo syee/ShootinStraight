652d14e1b57ffe44d9fbf2173641f1b4aa3cb5e4
Steven Yee <steven.yee@cengage.com>
2015-12-05 17:55:48 -0500

    initial commit

---
+++ shootinStraight.py
@@ -0,0 +1,10 @@
+import goldsberry
+import pandas as pd
+
+pd.set_option("display.max_columns", 50)
+print goldsberry.__version__
+
+
+
+# playersCurrent = pd.DataFrame(goldsberry.PlayerList(2014))
+# playersCurrent.head()
